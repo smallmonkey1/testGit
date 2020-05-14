2ff592642a9fd1d424590f099db804e96db87a0a
smallmonkey1 <1226408278@qq.com>
2020-05-14 13:47:37 +0800

    My first commit

---
+++ src/main/java/com/zfl/git/Apple.java
@@ -0,0 +1,8 @@
+package com.zfl.git;
+/*
+@author author ZFL:
+@version time:2020年5月14日下午12:47:48
+*/
+public class Apple {
+
+}
---
+++ src/test/java/com/zfl/git/GoodTest.java
@@ -0,0 +1,9 @@
+package com.zfl.git;
+/*
+@author author ZFL:
+@version time:2020年5月14日下午12:48:39
+*/
+public class GoodTest {
+
+}
+ 
